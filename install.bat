c878ef1d358497c28cfe05dd1f5aafbc7062491f
Nonja <anantakshg@gmail.com>
2023-04-02 21:43:16 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 @echo off
-@title installer
+@title installer Chess.Com Stat Tracker & Auto q
 pip install -r requirements.txt
 cls
+echo Starting Stat Tracker if it opens a vsc window or anything just close it and open the STATS-TRACKER.py file normally
+START STATS-TRACK.PY
 echo installed click enter to exit!
 pause -nul
